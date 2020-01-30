7133203ff514c220dbbdac71e0a9e821c1cb67aa
Keith Kabza <admin@instpod.onmicrosoft.com>
2020-01-30 00:22:50 -0500

    exercise6

---
+++ Exercise06_ReactAppGraphAPI_COMPLETED/src/Config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  appId: '765634d2-ff2d-42da-92f1-b930f6234c20',
+  redirectUri: 'http://localhost:3000',
+  scopes: [
+    'user.read',
+    'presence.read.all',
+    'calendars.read'
+  ]
+};
